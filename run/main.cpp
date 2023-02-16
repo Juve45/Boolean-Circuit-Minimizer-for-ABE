5536e6c1290484dca5e6d0394cb35e3d14232d02
Gareth618 <bloggareth@gmail.com>
2023-02-16 19:31:13 +0200

    extended boolean formulas

--- run/main.cpp
+++ run/main.cpp
@@ -3,12 +3,19 @@
 #include "../headers/patterns.h"
 
 int main() {
-    std::string formula1 = "((((a+b)*(b+c))*d)+(a+b))";
+    std::string formula1 = "((((a+b+c)*(b+c))*d)+(a+b+c)+b+c)";
     Circuit circuit = Utils::to_circuit(formula1);
     std::string formula2 = Utils::to_formula(circuit);
     std::cout << formula1 << "\n\n";
     std::cout << circuit << '\n';
     std::cout << formula2 << '\n';
+
+    // std::string formula1 = "((((a+b)*(b+c))*d)+(a+b))";
+    // Circuit circuit = Utils::to_circuit(formula1);
+    // std::string formula2 = Utils::to_formula(circuit);
+    // std::cout << formula1 << "\n\n";
+    // std::cout << circuit << '\n';
+    // std::cout << formula2 << '\n';
 
     // std::vector<Subcircuit> patterns = {pattern1, pattern2};
     // std::vector<Subcircuit> replacements = {replacement1, replacement2};
--- sources/Utils.cpp
+++ sources/Utils.cpp
@@ -42,28 +42,35 @@ Circuit& Utils::to_circuit(const std::string& formula) {
     std::vector<std::string> stack;
     std::map<std::string, Node*> mapping;
     for (const char chr : formula) {
-        if (chr == '*' || chr == '+' || ('a' <= chr && chr <= 'z'))
-            stack.emplace_back(1, chr);
+        stack.emplace_back(1, chr);
         if ('a' <= chr && chr <= 'z' && !mapping.count(stack.back())) {
             mapping[stack.back()] = new Node(INPUT);
             leaves.push_back(mapping[stack.back()]);
         }
         else if (chr == ')') {
-            const std::string lhs = stack[stack.size() - 3];
-            const std::string opr = stack[stack.size() - 2];
-            const std::string rhs = stack[stack.size() - 1];
-            std::string subformula = "(" + lhs + opr + rhs + ")";
-            if (!mapping.count(subformula)) {
-                mapping[subformula] = new Node(opr == "*" ? AND : OR);
-                Node *lhs_node = mapping[lhs];
-                Node *rhs_node = mapping[rhs];
-                Node *big_node = mapping[subformula];
-                big_node->lower.insert(lhs_node); lhs_node->upper.insert(big_node);
-                big_node->lower.insert(rhs_node); rhs_node->upper.insert(big_node);
+            std::string operation;
+            std::vector<std::string> operands;
+            while (true) {
+                const std::string top = stack.back();
+                stack.pop_back();
+                if (top == "(") break;
+                if (top == "*" || top == "+") operation = top;
+                operands.push_back(stack.back());
+                stack.pop_back();
             }
-            stack.pop_back();
-            stack.pop_back();
-            stack.pop_back();
+            std::string subformula = "(";
+            for (int i = operands.size() - 1; i >= 0; i--)
+                subformula += operands[i] + operation;
+            subformula.back() = ')';
+            if (!mapping.count(subformula)) {
+                mapping[subformula] = new Node(operation == "*" ? AND : OR);
+                Node *upper_node = mapping[subformula];
+                for (const auto& operand : operands) {
+                    Node *lower_node = mapping[operand];
+                    upper_node->lower.insert(lower_node);
+                    lower_node->upper.insert(upper_node);
+                }
+            }
             stack.push_back(subformula);
         }
     }
@@ -89,13 +96,13 @@ std::string Utils::to_formula(const Circuit& circuit) {
     std::map<Node*, std::string> mapping;
     std::function<std::string(Node*)> dfs = [&](Node* node) {
         if (!mapping.count(node)) {
-            if (node->type == AND || node->type == OR)
-                mapping[node]
-                    = "("
-                    + dfs(*(node->lower.begin()))
-                    + (node->type == AND ? '*' : '+')
-                    + dfs(*(node->lower.rbegin()))
-                    + ")";
+            if (node->type == AND || node->type == OR) {
+                std::string subformula = "(";
+                for (Node* lower_node : node->lower)
+                    subformula += dfs(lower_node) + (node->type == AND ? '*' : '+');
+                subformula.back() = ')';
+                mapping[node] = subformula;
+            }
             else if (node->type == FAN_OUT)
                 mapping[node] = dfs(*(node->lower.begin()));
             else
